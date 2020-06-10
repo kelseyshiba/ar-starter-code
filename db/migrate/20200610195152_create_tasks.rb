564c133e46c81ccb5b6956c94c506203479214b4
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-06-10 14:18:49 -0600

    Create tables and relationships

---
+++ db/migrate/20200610195152_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.boolean :done
+      t.datetime :due_date
+      t.belongs_to :user
+      t.belongs_to :category
+    end
+  end
+end
